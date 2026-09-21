71a4d84f5876afe6cc01b215f9dddb6b3f1f5144
charlesnoah344 <23397@ecam.be>
2026-09-21 16:52:51 +0200

    config

---
+++ config.js
@@ -0,0 +1,2 @@
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxGWLUAr46aIYCHy4kBNBjPXffVbt-NqlMYhz8izYRp_CHc-Od_hYnG-IewYvwfGOLM/exec';
+const RSVP_TOKEN = 'b7dabfc3a8ec4122a10699e52fb740e7';
