d0b97dc275445c940f43e9eefcd07fea480b944a
charlesnoah344 <23397@ecam.be>
2026-09-23 22:11:36 +0200

    corrections finales qr et controles 3

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxGWLUAr46aIYCHy4kBNBjPXffVbt-NqlMYhz8izYRp_CHc-Od_hYnG-IewYvwfGOLM/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzldfZXlHOVQBzFlKurTxPDP-no_24RbeJFVaqQWYYq3t-1I-uBjPPx7r5s5i_Dyh6q/exec';
 const RSVP_TOKEN = 'b7dabfc3a8ec4122a10699e52fb740e7';
